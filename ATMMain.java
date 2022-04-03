5b10b89e6aa50a2ec572e1a0bb2a8b3c80eb6998
aininurulazizah <aini.nurul.tif421@polban.ac.id>
2022-04-03 10:14:51 +0700

    First Commit Task W7

---
+++ ATMMain.java
@@ -0,0 +1,15 @@
+
+
+ // Aini Nurul Azizah || 211524034
+
+public class ATMMain {
+   // main method creates and runs the ATM
+   public static void main(String[] args) {
+      ATM theATM = new ATM();    
+      theATM.run();
+   }
+} 
+
+
+
+ 
